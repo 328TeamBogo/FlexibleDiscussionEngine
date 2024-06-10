ebd0c65fbd11917ab4a2cab1d324f58e08b28f2d
Reshad Yadgari <ahmadreshadyadgari@gmail.com>
2024-06-10 01:07:47 -0400

    validate sign up SQL to ensure user enter characters only.

--- model/validate.php
+++ model/validate.php
@@ -1,1 +1,19 @@
 <?php
+
+class Validate {
+    /**
+     * Validate username
+     */
+    public static function validUsername($username) {
+        // Username must be at least 3 characters long and contain only letters
+        return preg_match('/^[a-zA-Z]{3,}$/', $username);
+    }
+
+    /**
+     * Validate password
+     */
+    public static function validPassword($password) {
+        // Password must be at least 6 characters long and contain only letters
+        return preg_match('/^[a-zA-Z]{6,}$/', $password);
+    }
+}
